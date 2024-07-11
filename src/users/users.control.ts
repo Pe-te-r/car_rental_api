17726cf86483e797e15bf39e5c1bd940a2cfc70e
Pe-te-r <mburupeter8526@gmail.com>
2024-07-11 20:32:10 +0300

    better version

--- src/users/users.control.ts
+++ src/users/users.control.ts
@@ -1,11 +1,15 @@
 
 import { Context } from "hono";
 import { addUserDetails, deletUsersDetails, getAllUserDetails, getOneUserDetails, updateUserDetails } from "./users.services";
+import { verifyToken } from "../middle_auth/middleware";
 
 
 export const getUser=async(c: Context)=>{
     const id = c.req.param('id')
+    const token = c.req.header("Authorization");
+    const decoded= await verifyToken(token as string,process.env.SECRET_KEY as string);
     if(isNaN(Number(id))) return c.json({"message":"insert a valid id"})
+    if(Number(decoded?.user_id) !== Number(id) || !decoded) return c.json({msg:"cannot get another user details"})
     const result = await getOneUserDetails(Number(id))
     if(result===undefined) return c.json({"message":`no such user with id ${id}`})
     return c.json({"results" : result})
@@ -38,9 +42,12 @@ export const addUser=async(c: Context)=>{
 // controller for update a user
 export const updateUser=async(c: Context)=>{
     try {
-        const newDetails= await c.req.json()
         const id = c.req.param('id')
         if(isNaN(Number(id))) return c.json({"message":"insert a valid id"})
+        const token = c.req.header("Authorization");
+        const decoded = await verifyToken(token!,process.env.SECRET_KEY!)
+        if(decoded?.user_id !== id && decoded?.role != 'admin') return c.json({ "msg": "cannot update this details"})
+        const newDetails= await c.req.json()
         const results = await updateUserDetails(Number(id),newDetails)
         return c.json({'message': results})
     } catch (error: any) {
@@ -51,7 +58,9 @@ export const updateUser=async(c: Context)=>{
 export const deleteUser=async(c: Context)=>{
     try {
         const id = c.req.param('id')
-        console.log(id)
+        const token = c.req.header("Authorization");
+        const decoded= await verifyToken(token as string,process.env.SECRET_KEY as string);
+        if(Number(decoded?.user_id && decoded?.role !== "admin") !== Number(id) || !decoded) return c.json("cannot delete another user")
         if(isNaN(Number(id))) return c.json({"message":"insert a valid id"})
         const results = await deletUsersDetails(Number(id))
         return c.json({'message': results})
